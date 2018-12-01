2a29152493eed63233a13aa234e78bb34ce4066e
Noam <noamtayri1990@gmail.com>
2018-12-01 23:19:22 +0200

    finnish k_means algorithm without tests

--- src/main/java/watchdog_package/logic/K_Means.java
+++ src/main/java/watchdog_package/logic/K_Means.java
@@ -1,14 +1,14 @@
 package main.java.watchdog_package.logic;
 
-import javafx.geometry.Pos;
 import main.java.watchdog_package.entities.Location;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class K_Means {
     private int numOfClusters;
-    private List<Point> pointsList;
-    private List<Cluster> clusters;
+    private List<Point> pointsList = new ArrayList<>();
+    private List<Cluster> clusters = new ArrayList<>();
     private double radius;
 
     public K_Means(List<Location> locationsList, double radius){
@@ -17,6 +17,24 @@ public class K_Means {
         }
         numOfClusters = 1;
         this.radius = radius;
+    }
+
+    public List<Cluster> run(){
+        boolean allClustersInRadius = false;
+        while(!allClustersInRadius){
+            initClusters();
+            boolean pointsDidNotSwitchClusters = false;
+            while(!pointsDidNotSwitchClusters){
+                pointsDidNotSwitchClusters = groupPoints();
+                calculateClustersCenters();
+            }
+            allClustersInRadius = isAllClusterInRadiusRange();
+            if(!allClustersInRadius){
+                numOfClusters++;
+                clusters.clear();
+            }
+        }
+        return clusters;
     }
 
     private void initClusters(){
@@ -46,6 +64,119 @@ public class K_Means {
         return noChange;
     }
 
+    private void calculateClustersCenters(){
+        for (Cluster cluster:
+             clusters) {
+            List<Position> positionsInCurrentCluster = new ArrayList<>();
+            for (Point point:
+                 pointsList) {
+                if(point.myCluster == cluster.id){
+                    positionsInCurrentCluster.add(point.position);
+                    cluster.numOfPoints++;
+                }
+            }
+            cluster.center = midpoint(positionsInCurrentCluster);
+        }
+    }
+
+    private boolean isAllClusterInRadiusRange(){
+        for (Cluster cluster:
+             clusters) {
+            for (Point point:
+                 pointsList) {
+                if(point.myCluster == cluster.id){
+                    if(distance(point.position, cluster.center) > radius){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Calculation of the geographic midpoint (also known as the geographic center,
+     * or center of gravity) for two or more points on the earth's surface Ref:
+     * http://www.geomidpoint.com/calculation.html
+     *
+     * @param points
+     * @return
+     */
+    private Position midpoint(List<Position> points) {
+        double Totweight = 0;
+        double xt = 0;
+        double yt = 0;
+        double zt = 0;
+        for (Position point : points) {
+            Double latitude = point.lat;
+            Double longitude = point.lon;
+
+            /**
+             * Convert Lat and Lon from degrees to radians.
+             */
+            double latn = latitude * Math.PI / 180;
+            double lonn = longitude * Math.PI / 180;
+
+            /**
+             * Convert lat/lon to Cartesian coordinates
+             */
+            double xn = Math.cos(latn) * Math.cos(lonn);
+            double yn = Math.cos(latn) * Math.sin(lonn);
+            double zn = Math.sin(latn);
+
+            /**
+             * Compute weight (by time) If locations are to be weighted equally,
+             * set wn to 1
+             */
+            double years = 0;
+            double months = 0;
+            double days = 0;
+            double wn = true ? 1 : (years * 365.25) + (months * 30.4375) + days;
+
+            /**
+             * Compute combined total weight for all locations.
+             */
+            Totweight = Totweight + wn;
+            xt += xn * wn;
+            yt += yn * wn;
+            zt += zn * wn;
+        }
+
+        /**
+         * Compute weighted average x, y and z coordinates.
+         */
+        double x = xt / Totweight;
+        double y = yt / Totweight;
+        double z = zt / Totweight;
+
+        /**
+         * If abs(x) < 10-9 and abs(y) < 10-9 and abs(z) < 10-9 then the
+         * geographic midpoint is the center of the earth.
+         */
+        double lat = -0.001944;
+        double lon = -78.455833;
+        if (Math.abs(x) < Math.pow(10, -9) && Math.abs(y) < Math.pow(10, -9) && Math.abs(z) < Math.pow(10, -9)) {
+        } else {
+
+            /**
+             * Convert average x, y, z coordinate to latitude and longitude.
+             * Note that in Excel and possibly some other applications, the
+             * parameters need to be reversed in the atan2 function, for
+             * example, use atan2(X,Y) instead of atan2(Y,X).
+             */
+            lon = Math.atan2(y, x);
+            double hyp = Math.sqrt(x * x + y * y);
+            lat = Math.atan2(z, hyp);
+
+            /**
+             * Convert lat and lon to degrees.
+             */
+            lat = lat * 180 / Math.PI;
+            lon = lon * 180 / Math.PI;
+        }
+        return new Position(lat, lon);
+    }
+
     // distance returned in METERS
     private double distance(Position p1, Position p2) {
         int r = 6371; //Radius of the earth in km
@@ -58,10 +189,7 @@ public class K_Means {
         return d * 1000;
     }
 
-
-
-
-    private class Cluster{
+    public class Cluster{
         private int id;
         private int numOfPoints;
         private Position center;
@@ -70,6 +198,10 @@ public class K_Means {
             this.id = id;
             numOfPoints = 0;
             center = position;
+        }
+
+        public Position getCenter() {
+            return center;
         }
     }
 
@@ -86,9 +218,14 @@ public class K_Means {
     private class Position{
         public double lat;
         public double lon;
-        public Position(double let, double lon){
-            this.lat = let;
+        public Position(double lat, double lon){
+            this.lat = lat;
             this.lon = lon;
+        }
+
+        @Override
+        public String toString() {
+            return "lat = " + lat + ", lon = " + lon;
         }
     }
 }
